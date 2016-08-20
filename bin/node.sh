50f534c8027c43c93e20ae45cfab94b4a7e27764
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-08-20 16:38:14 +0100

    Add more react npm dependencies

--- bin/node.sh
+++ bin/node.sh
@@ -11,23 +11,26 @@ modules=(
     babel-core
     babel-eslint
     babel-loader
+    babel-loader
     babel-preset-es2015
     babel-preset-react
+    brunch
+    elm-brunch
+    elm-oracle
+    eslint
     eslint-config-standard
     eslint-config-standard-jsx
     eslint-config-standard-react
     eslint-plugin-promise
     eslint-plugin-react
     eslint-plugin-standard
-    brunch
-    elm-brunch
-    elm-oracle
-    eslint
     jest-cli
     js-beautify
     phantomjs
     react
     react-dom
+    react-redux
+    redux
     standard
     tern
     webpack
