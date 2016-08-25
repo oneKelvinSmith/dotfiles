cdd3b9aa0beee2bbc51adefec6570cf7e1dd70ab
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-08-25 10:54:34 +0100

    Stop overriding coreutils

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -28,9 +28,6 @@ set -gx LC_ALL "en_US.UTF-8"
 
 set -gx PATH $HOME/.bin $PATH
 
-set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
-
 set -gx OMF_PATH $HOME/.local/share/omf
 source $OMF_PATH/init.fish
 
