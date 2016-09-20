32685cbc56fb042ef20aaad880dfb44ca832286b
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-09-20 14:56:41 +0100

    Make Code function smart

--- config/fish/functions/code.fish
+++ config/fish/functions/code.fish
@@ -1,3 +1,12 @@
 function code
-  cd ~/Code
+  if count $argv > /dev/null
+    set -l project (ls ~/Code | fzf -f $argv | head -1)
+    if test $project
+      cd ~/Code/(echo $project)
+    else
+      echo "No matching project found..."
+    end
+  else
+    cd ~/Code
+  end
 end
