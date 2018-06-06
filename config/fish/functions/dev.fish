0178d676611d6a6b8ca39d94bea5d51f585467cb
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-06-06 15:41:53 +0100

    Update dev function

--- config/fish/functions/dev.fish
+++ config/fish/functions/dev.fish
@@ -1,12 +1,15 @@
 function dev
+  set -l directory ~/Development
+
   if count $argv > /dev/null
-    set -l project (ls ~/Code | fzf -f $argv | head -1)
+    set -l project (ls (echo $directory) | fzf -f $argv | head -1)
+
     if test $project
-      cd ~/Code/(echo $project)
+      cd (echo $directory)/(echo $project)
     else
       echo "No matching project found..."
     end
   else
-    cd ~/Code
+    cd (echo $directory)
   end
 end
