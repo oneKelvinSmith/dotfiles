4b4774380ee069091c40d74b1645e63aeb202c1a
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-06-06 16:29:57 +0100

    Move to gruvbox palette in terminal

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,26 +1,26 @@
 set fish_greeting ""
 
-set fish_color_autosuggestion 75715E
-set fish_color_command A6E22E
-set fish_color_comment E6DB74
-set fish_color_cwd A6E22E
-set fish_color_cwd_root F92672
+set fish_color_autosuggestion 928374
+set fish_color_command 8ec07c
+set fish_color_comment a89984
+set fish_color_cwd fabd2f
+set fish_color_cwd_root d79921
 set fish_color_end normal
-set fish_color_error F92672
-set fish_color_escape	75715E
-set fish_color_history_current FD971F
-set fish_color_host	66D9EF
-set fish_color_match A6E22E
+set fish_color_error fb4934
+set fish_color_escape	928374
+set fish_color_history_current d65d0e
+set fish_color_host	458588
+set fish_color_match b8bb26
 set fish_color_normal normal
-set fish_color_operator	66D9EF
-set fish_color_param FD971F
-set fish_color_quote E6DB74
+set fish_color_operator	83a598
+set fish_color_param normal
+set fish_color_quote b8bb26
 set fish_color_redirection normal
 set fish_color_search_match normal
-set fish_color_status F92672
-set fish_color_user -o A6E22E
+set fish_color_status b16286
+set fish_color_user -o d3869b
 set fish_color_valid_path --underline
-set fish_pager_color_progress 66D9EF
+set fish_pager_color_progress fe8019
 
 set -gx EDITOR    nvim
 set -gx COLORTERM truecolor
