027da9d020b4e9819ecd1e160de4489609cd266d
Kelvin Smith <oneKelvinSmith@gmail.com>
2018-06-08 17:19:01 +0100

    Add yarn binaries to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -30,9 +30,11 @@ source $HOME/.asdf/asdf.fish
 set -gx CARGOPATH $HOME/.cargo
 set -gx GOPATH    $HOME/.go
 set -gx GOROOT    (go env GOROOT)
+set -gx YARNPATH  (yarn global dir)/node_modules/
 
 set -g fish_user_paths $CARGOPATH/bin  $fish_user_paths
 set -g fish_user_paths $GOPATH/bin     $fish_user_paths
 set -g fish_user_paths $GOROOT/bin     $fish_user_paths
+set -g fish_user_paths $YARNPATH/.bin  $fish_user_paths
 set -g fish_user_paths $HOME/.bin      $fish_user_paths
 set -g fish_user_paths /usr/local/sbin $fish_user_paths
