dde94c505d5d04ee8bf3927d90797af6b0809e77
Kelvin Smith <oneKelvinSmith@gmail.com>
2020-05-14 16:11:37 +0100

    Remove oh-my-fish and simplify prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,38 +1,17 @@
 set fish_greeting ""
 
-set fish_color_autosuggestion 928374
-set fish_color_command 8ec07c
-set fish_color_comment a89984
-set fish_color_cwd fabd2f
-set fish_color_cwd_root d79921
-set fish_color_end normal
-set fish_color_error fb4934
-set fish_color_escape 928374
-set fish_color_history_current d65d0e
-set fish_color_host 458588
-set fish_color_match b8bb26
-set fish_color_normal normal
-set fish_color_operator	83a598
-set fish_color_param normal
-set fish_color_quote b8bb26
-set fish_color_redirection normal
-set fish_color_search_match normal
-set fish_color_status b16286
-set fish_color_user -o d3869b
-set fish_color_valid_path --underline
-set fish_pager_color_progress fe8019
-
 set -gx EDITOR    nvim
 set -gx COLORTERM truecolor
 
 source (brew --prefix asdf)/asdf.fish
 
-set -gx GOPATH    $HOME/.go
-set -gx GOROOT    (go env GOROOT)
-set -gx YARNPATH  (yarn global dir)/node_modules/
+set -gx GOPATH   $HOME/.go
+set -gx GOROOT   (go env GOROOT)
+set -gx YARNPATH (yarn global dir)/node_modules/
 
-set -g fish_user_paths $GOPATH/bin      $fish_user_paths
-set -g fish_user_paths $GOROOT/bin      $fish_user_paths
-set -g fish_user_paths $YARNPATH/.bin   $fish_user_paths
-set -g fish_user_paths $HOME/.local/bin $fish_user_paths
-set -g fish_user_paths /usr/local/sbin  $fish_user_paths
+set -gx PATH $GOPATH/bin      $PATH
+set -gx PATH $GOROOT/bin      $PATH
+set -gx PATH $YARNPATH/.bin   $PATH
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH /usr/local/sbin  $PATH
+set -gx PATH /usr/local/opt/mongodb-community@3.4/bin $PATH
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,60 +1,15 @@
-function fish_prompt
-  set -l last_command_status $status
-  set -l cwd
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
 
-  if test "$theme_short_path" = 'yes'
-    set cwd (basename (prompt_pwd))
-  else
-    set cwd (prompt_pwd)
-  end
+    # For git prompt integration see:
+    # https://fishshell.com/docs/current/cmds/fish_git_prompt.html
 
-  set -l dollar   "\$ "
-  set -l chevron  ">"
-  set -l ahead    "↑"
-  set -l behind   "↓"
-  set -l diverged "⥄ "
-  set -l dirty    "⨯"
-  set -l none     "◦"
-
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
-  set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
-
-  if test $last_command_status -eq 0
-    echo -n -s $success_color $chevron $normal_color
-  else
-    echo -n -s $error_color $chevron $normal_color
-  end
-
-  if git_is_repo
-    if test "$theme_short_path" = 'yes'
-      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-      set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-
-      echo -n -s " " $directory_color $cwd $normal_color
+    if not test $last_status -eq 0
+        set_color $fish_color_error
     else
-      echo -n -s " " $directory_color $cwd $normal_color
+	set_color $fish_pager_color_progress
     end
 
-    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
-
-    if git_is_touched
-      echo -n -s $dirty
-    else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
-    end
-  else
-    echo -n -s " " $directory_color $cwd $normal_color
-  end
-
-  echo -n -s " "
-
-  if test $last_command_status -eq 0
-      echo -e "\n$success_color$dollar$normal_color"
-  else
-      echo -e "\n$error_color$dollar$normal_color"
-  end
+    echo -n '$ '
+    set_color normal
 end
